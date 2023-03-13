6bb016f856484ac911cafb20d8713db0324a8cf9
Will Sharp <sharpie@udel.edu>
2023-03-13 14:38:24 -0400

    Finished tests for changeType and RevealAnswer

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questType, setQuestType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeQuestionType(): void {
+        setQuestType(
+            questType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    function whatQuestion(type: QuestionType): string {
+        return type === "multiple_choice_question"
+            ? "Multiple Choice"
+            : "Short Answer";
+    }
+    return (
+        <div>
+            <div>
+                Current Question: <span>{whatQuestion(questType)}</span>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+            </div>
+        </div>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [hidden, setHidden] = useState<boolean>(false);
+
+    function flipHidden(): void {
+        setHidden(!hidden);
+    }
+
+    return (
+        <div>
+            <Button onClick={flipHidden}>Reveal Answer</Button>
+            {hidden && <div>42</div>}
+        </div>
+    );
 }

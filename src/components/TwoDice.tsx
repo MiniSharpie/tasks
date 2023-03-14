58aa0fc3844ede2c8a73fe8cd6a1093bd04d2164
Will Sharp <sharpie@udel.edu>
2023-03-13 21:26:40 -0400

    finished TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,40 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [left_die, setDie1] = useState<number>(1);
+    const [right_die, setDie2] = useState<number>(2);
+
+    function isWin(): boolean {
+        if (left_die === right_die && left_die != 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function isLoss(): boolean {
+        if (right_die === 1 && left_die === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return (
+        <div>
+            <span>
+                Left die:<span data-testid="left-die">{left_die}</span>
+            </span>
+            <span>
+                Right die:<span data-testid="right-die">{right_die}</span>
+            </span>
+            <Button onClick={() => setDie1(d6())}>Roll Left</Button>
+            <Button onClick={() => setDie2(d6())}>Roll Right</Button>
+            <div>
+                {isLoss() ? (
+                    <span> You Lose</span>
+                ) : isWin() ? (
+                    <span>You Win</span>
+                ) : null}
+            </div>
+        </div>
+    );
 }

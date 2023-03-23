7e575838b84df5fbb66706ea471eedad1fcde06c
Will Sharp <sharpie@udel.edu>
2023-03-23 13:24:36 -0400

    Passing tests for CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
+
+    function isRight(answer: string): string {
+        if (answer === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formUserAnswer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    value={userAnswer}
+                    onChange={updateAnswer}
+                ></Form.Control>
+            </Form.Group>
+            <div>
+                Answer: <span>{isRight(userAnswer)}</span>
+            </div>
         </div>
     );
 }

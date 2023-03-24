f056e07829f38feb8e4222f81bc7ebd552f4b435
Will Sharp <sharpie@udel.edu>
2023-03-23 23:39:41 -0400

    Passing all tests for task 10

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,33 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userChoice, setUserChoice] = useState<string>(options[0]);
+
+    function isAnswer(answer: string): string {
+        if (answer === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setUserChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="pick-option">
+                <Form.Label>Pick an Option:</Form.Label>
+                <Form.Select value={userChoice} onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>Your answer is: {isAnswer(userChoice)}</div>
         </div>
     );
 }
